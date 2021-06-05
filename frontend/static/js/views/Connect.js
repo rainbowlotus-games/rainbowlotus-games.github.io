95fd111835936d65775e41381900595065e07375
eidens <rainbowlotus-games@gmail.com>
2021-06-05 04:21:44 +0200

    Remove projectId from connect page

--- frontend/static/js/views/Connect.js
+++ frontend/static/js/views/Connect.js
@@ -3,12 +3,11 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.projectId = params.id;
         this.setTitle("Connect with me");
     }
 
     async getHtml() {
-        return /*html*/ `
+        return /*html*/ `           
             <h1>Connect</h1>
             <p>Message me on any of these things!</p>
         `;
